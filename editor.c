8471c3108bfc292cf1af4fea28cedb7672961cb3
David Bors <davidbors@icloud.com>
2021-04-04 13:42:40 +0300

    added gl ; dl

--- editor.c
+++ editor.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "micro.h"
 #include "stiva.h"
 
@@ -7,18 +8,18 @@
  * DONE q -> quit
  * DONE ::i -> switcher
  * insert chars -> done
- * u
- * r
  * DONE s -> save the document
  * DONE b -> del the last char before pointer
- * dl [line]
- * gl line
+ * DONE dl [line] -> del either the current line or line number [line]
+ * DONE gl line -> set cursor on line
  * gc char [line]
  * d [chars]
- * re old_word new_word
- * ra old_word new_word
  * dw word
  * da word  
+ * re old_word new_word
+ * ra old_word new_word
+ * u
+ * r
  */
 
 int main(){
@@ -59,7 +60,7 @@ int main(){
             
         } else {
             if(state){
-                // printf("insert %c @ %d %d\n", counter->data, PY, PX);
+                printf("insert %c @ %d %d\n", counter->data, PY, PX);
                 text = ins(text, PY, PX, counter->data);
                 if(counter->data == '\n'){
                     PPX = PX;
@@ -84,7 +85,39 @@ int main(){
                 }
                 else if(counter->data == '\n'){
                     printf("ok.\n");
-                }
+                } else if(counter->data == 'd' && counter->next->data == 'l') {
+                    counter = counter->next->next;
+                    if(counter && counter->data == '\n'){
+                        // se sterge linia
+                        text = del(text, PY, -1);
+                    } else if(counter && counter->data == ' '){
+                        Node *cc;
+                        cc = counter->next;
+                        int x = 0;
+                        int p = 1;
+                        while(cc && cc->data != '\n'){
+                            printf("haha: %d ; %d\n", atoi(&cc->data), x);
+                            x = x * p + atoi(&cc->data);
+                            p *= 10;
+                            cc = cc->next;
+                        }
+                        text = del(text, x, -1);
+                    }
+                } else if(counter->data == 'g' && counter->next->data == 'l' &&
+                    counter->next->next->data == ' '){
+                        Node *cc;
+                        cc = counter->next;
+                        int x = 0;
+                        int p = 1;
+                        while(cc && cc->data != '\n'){
+                            printf("haha: %d ; %d\n", atoi(&cc->data), x);
+                            x = x * p + atoi(&cc->data);
+                            p *= 10;
+                            cc = cc->next;
+                        }
+                        printf("%d\n", x);
+                        chg(0, x-1);
+                    }
                 if(counter != NULL)
                     counter = counter->next;
             }
@@ -92,5 +125,26 @@ int main(){
     }
 
     printList(text);
+
+    // text = ins(text, 0, 0, 'a');
+    // text = ins(text, 0, 1, 'a');
+    // text = ins(text, 0, 2, 'a');
+    // text = ins(text, 0, 3, '\n');
+    // text = ins(text, 1, 0, 'b');
+    // text = ins(text, 1, 1, 'b');
+    // text = ins(text, 1, 2, 'b');
+    // text = ins(text, 1, 3, '\n');
+    // text = ins(text, 2, 0, 'c');
+    // text = ins(text, 2, 1, 'c');
+    // text = ins(text, 2, 2, 'c');
+    // text = ins(text, 2, 3, '\n');
+
+    // printList(text);
+
+    // text = ins(text, 1, 3, 't');
+    // text = ins(text, 1, 4, 'e');
+    // text = ins(text, 1, 5, 's');
+    // text = ins(text, 1, 6, 't');
+    // printList(text);
     return 0;
 }
--- micro.h
+++ micro.h
@@ -68,11 +68,16 @@ Node *del(Node *text, int i, int j){
  * argumentul data. Returneaza capul listei dublu inlantuite.
 */
 Node *ins(Node *text, int i, int j, char data){
-    Node *p, *aux;
+    Node *p, *aux, *auxx;
+    p = NULL;
+    aux = NULL;
+    auxx = NULL;
+
     int auxi, auxj;
     p = text;
     auxi = i;
     auxj = j;
+
     if(i == 0 && j == 0){
         Node *toAdd = init_node(data);
         toAdd->back = NULL;
@@ -83,48 +88,61 @@ Node *ins(Node *text, int i, int j, char data){
     }
     while(auxi && p){
         if(p->data == '\n') --auxi;
+        auxx = aux;
         aux = p;
         p = p->next;
     }
     // printf("first aux: %d\n", auxi);
-    if(auxi >= 1 && p == NULL){
-        printf("Nu se poate insera decat maxim o pozitie in plus in afara textului existent! err 1\n");
+    if(auxi > 0 && p == NULL){
+        printf("Nu se poate insera decat maxim o pozitie in plus in afara\
+         textului existent! err 1\n");
         return text; 
 
     }
     if(auxi == 0 && p == NULL){
         if(auxj > 0){
-            printf("Nu se poate insera decat maxim o pozitie in plus in afara textului existent! err 3\n");
+            printf("Nu se poate insera decat maxim o pozitie in plus in afara\
+             textului existent! err 3\n");
             return text;
         }
         Node *toAdd = init_node(data);
         toAdd->back = aux;
-        toAdd->next = p;
+        toAdd->next = NULL;
         aux->next = toAdd;
         return text;
     } else {
-    while(auxj && p){
+    short int ok = 1;
+    while(auxj && p && ok){
+        auxx = aux;
         aux = p;
         p = p->next;
         if(p != NULL && p->data == '\n'){
-            aux = p;
-            p = NULL;
+            ok = 0;
         }
         auxj--;
     }
     // printf("%d\n", auxj);
     if(auxj > 0 && p == NULL){
-        printf("Nu se poate insera decat maxim o pozitie in plus in afara textului existent! err 2\n");
+        printf("Nu se poate insera decat maxim o pozitie in plus in afara\
+         textului existent! err 2\n");
         return text; 
     }
-    if(p!=NULL){
+    if(p != NULL && !ok) {
+        // printf("this case!\n");
+        Node *toAdd = init_node(data);
+        toAdd->next = p;
+        aux->next = toAdd;
+        toAdd->back = aux;    
+        p->back = toAdd;
+    }
+    else if(p!=NULL){
     //printf("wtf?!");
     Node *toAdd = init_node(data);
-    toAdd->next = p->next;
-    toAdd->back = p;
-    if(p->next != NULL)
-        p->next->back = toAdd;
-    p->next = toAdd;
+    toAdd->next = p;
+    toAdd->back = aux;
+    if(aux != NULL)
+        aux->next = toAdd;
+    p->back = toAdd;
     } else {
         //if(aux == NULL) printf("wtf\n");
         //printf("wtf?!");

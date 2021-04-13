fdbfb83efdf29d7f6947b72a521af7132f830792
David Bors <davidbors@icloud.com>
2021-04-13 21:00:18 +0300

    hopefully got rid of valgrind

--- micro.h
+++ micro.h
@@ -11,55 +11,101 @@ int PX, PY;
  * linia i. returneaza primul nod al listei dublu inlantuite in care este tinut
  * textul.
 */
-Node *del(Node *text, int i, int j){
+Node *del(Node *text, int i, int j)
+{
     Node *p;
     int auxi, auxj;
     p = text;
     auxi = i;
-    auxj = j-1;
-    while(auxi && p){
-        if(p->data == '\n') --auxi;
+    auxj = j - 1;
+    while (auxi && p)
+    {
+        if (p->data == '\n')
+            --auxi;
         p = p->next;
     }
-    if(auxi >= 0 && p == NULL){
+    if (auxi >= 0 && p == NULL)
+    {
         printf("Erroare: nu exista linia data pentru stergere!\n");
         return text;
     }
-    if(j == -1){
-        if(i){
-        Node *aux = p->back;
-        Node *paux = p;
-        while(auxj && p){
-            if(p->data == '\n') auxj=0;
-            p = p->next;
-            paux = p;
+    if (j == -1)
+    {
+        if (i)
+        {
+            Node *aux = p->back;
+            Node *paux = p;
+            Node *auxx = NULL;
+            while (auxj && p)
+            {
+                if (p->data == '\n')
+                    auxj = 0;
+                p = p->next;
+                paux = p;
+            }
+            if (p != NULL)
+            {
+                auxx = aux;
+                aux = aux->next;
+                while (aux != p)
+                {
+                    paux = aux;
+                    aux = aux->next;
+                    free(paux);
+                }
+                auxx->next = p;
+                p->back = auxx;
+            }
+            else
+            {
+                auxx = aux;
+                aux = aux->next;
+                while (aux)
+                {
+                    paux = aux;
+                    aux = aux->next;
+                    free(paux);
+                }
+                auxx->next = NULL;
+            }
+            return text;
         }
-        if(p!=NULL){
-        p->back = aux;
-        aux->next = p;
-        } else aux->next = NULL;
-        return text;
-        } else {
-        while(auxj && p){
-            if(p->data == '\n') auxj=0;
-            p = p->next;
-        }   
-        return p;
+        else
+        {
+            Node *aux;
+            while (auxj && p)
+            {
+                aux = p;
+                if (p->data == '\n')
+                    auxj = 0;
+                p = p->next;
+                free(aux);
+            }
+            return p;
         }
-    } else {
-    if(i == 0 && j == 0)
-        return text->next;
-    while(auxj >= 0 && p){
-        p = p->next;
-        auxj--;
     }
-    if(auxj && p == NULL)
+    else
+    {
+        if (i == 0 && j == 0)
+        {
+            Node *aux = text;
+            text = text->next;
+            free(aux);
+            return text;
+        }
+        while (auxj >= 0 && p)
+        {
+            Node *aux = p;
+            p = p->next;
+            auxj--;
+        }
+        if (auxj && p == NULL)
+            return text;
+        p->back->next = p->next;
+        if (p->next)
+            p->next->back = p->back;
+        free(p);
         return text;
-    p->back->next = p->next;
-    if(p->next)
-        p->next->back = p->back;
-    free(p);
-    return text;
     }
 }
 
@@ -67,7 +113,8 @@ Node *del(Node *text, int i, int j){
  * Functie care insereaza in text, dupa pozitia i,j caracterul transmis prin 
  * argumentul data. Returneaza capul listei dublu inlantuite.
 */
-Node *ins(Node *text, int i, int j, char data){
+Node *ins(Node *text, int i, int j, char data)
+{
     Node *p, *aux, *auxx;
     p = NULL;
     aux = NULL;
@@ -78,29 +125,34 @@ Node *ins(Node *text, int i, int j, char data){
     auxi = i;
     auxj = j;
 
-    if(i == 0 && j == 0){
+    if (i == 0 && j == 0)
+    {
         Node *toAdd = init_node(data);
         toAdd->back = NULL;
         toAdd->next = text;
-	if(text != NULL)
-        	text->back = toAdd;
+        if (text != NULL)
+            text->back = toAdd;
         return toAdd;
     }
-    while(auxi && p){
-        if(p->data == '\n') --auxi;
+    while (auxi && p)
+    {
+        if (p->data == '\n')
+            --auxi;
         auxx = aux;
         aux = p;
         p = p->next;
     }
     // printf("first aux: %d\n", auxi);
-    if(auxi > 0 && p == NULL){
+    if (auxi > 0 && p == NULL)
+    {
         printf("Nu se poate insera decat maxim o pozitie in plus in afara\
          textului existent! err 1\n");
-        return text; 
-
+        return text;
     }
-    if(auxi == 0 && p == NULL){
-        if(auxj > 0){
+    if (auxi == 0 && p == NULL)
+    {
+        if (auxj > 0)
+        {
             printf("Nu se poate insera decat maxim o pozitie in plus in afara\
              textului existent! err 3\n");
             return text;
@@ -110,55 +162,65 @@ Node *ins(Node *text, int i, int j, char data){
         toAdd->next = NULL;
         aux->next = toAdd;
         return text;
-    } else {
-    short int ok = 1;
-    while(auxj && p && ok){
-        auxx = aux;
-        aux = p;
-        p = p->next;
-        if(p != NULL && p->data == '\n'){
-            ok = 0;
+    }
+    else
+    {
+        short int ok = 1;
+        while (auxj && p && ok)
+        {
+            auxx = aux;
+            aux = p;
+            p = p->next;
+            if (p != NULL && p->data == '\n')
+            {
+                ok = 0;
+            }
+            auxj--;
         }
-        auxj--;
-    }
-    // printf("%d\n", auxj);
-    if(auxj > 0 && p == NULL){
-        printf("Nu se poate insera decat maxim o pozitie in plus in afara\
+        // printf("%d\n", auxj);
+        if (auxj > 0 && p == NULL)
+        {
+            printf("Nu se poate insera decat maxim o pozitie in plus in afara\
          textului existent! err 2\n");
-        return text; 
+            return text;
+        }
+        if (p != NULL && !ok)
+        {
+            // printf("this case!\n");
+            Node *toAdd = init_node(data);
+            toAdd->next = p;
+            aux->next = toAdd;
+            toAdd->back = aux;
+            p->back = toAdd;
+        }
+        else if (p != NULL)
+        {
+            //printf("wtf?!");
+            Node *toAdd = init_node(data);
+            toAdd->next = p;
+            toAdd->back = aux;
+            if (aux != NULL)
+                aux->next = toAdd;
+            p->back = toAdd;
+        }
+        else
+        {
+            //if(aux == NULL) printf("wtf\n");
+            //printf("wtf?!");
+            Node *toAdd = init_node(data);
+            toAdd->next = aux->next;
+            aux->next = toAdd;
+            toAdd->back = aux;
+        }
     }
-    if(p != NULL && !ok) {
-        // printf("this case!\n");
-        Node *toAdd = init_node(data);
-        toAdd->next = p;
-        aux->next = toAdd;
-        toAdd->back = aux;    
-        p->back = toAdd;
-    }
-    else if(p!=NULL){
-    //printf("wtf?!");
-    Node *toAdd = init_node(data);
-    toAdd->next = p;
-    toAdd->back = aux;
-    if(aux != NULL)
-        aux->next = toAdd;
-    p->back = toAdd;
-    } else {
-        //if(aux == NULL) printf("wtf\n");
-        //printf("wtf?!");
-        Node *toAdd = init_node(data);
-        toAdd->next = aux->next;
-        aux->next = toAdd;
-        toAdd->back = aux;
-    }
-    }
-    return text; 
+    return text;
 }
 
 /*
  * functie care schimba coordonatele pointerului de scriere text
 */
-void chg(int i, int j){
+void chg(int i, int j)
+{
     PX = i;
     PY = j;
 }
@@ -166,10 +228,12 @@ void chg(int i, int j){
 /*
  * functie care salveaza o lista de caractere intr-un fisier
  */
-void save(Node *text, char *filename){
+void save(Node *text, char *filename)
+{
     FILE *fout = fopen(filename, "w");
     Node *counter = text;
-    while(counter != NULL){
+    while (counter != NULL)
+    {
         fprintf(fout, "%c", counter->data);
         counter = counter->next;
     }
